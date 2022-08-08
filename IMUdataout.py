32b9fd1645bfe2ebd19c25fc2368b6b5c643793c
katallen405 <95441393+katallen405@users.noreply.github.com>
2022-08-08 17:22:03 -0400

    html
    Not verified to work yet, developed without the RP2040 in hand

--- IMUdataout.py
+++ IMUdataout.py
@@ -1,47 +1,54 @@
-# Rename to main.py to run automatically on boot
 
 import time
 from lsm6dsox import LSM6DSOX
 import gc
-from machine import I2C, Pin
+from machine import I2C, Pin, Timer
 
 
-# Define the IMU 
-lsm = LSM6DSOX(I2C(0, scl=Pin(13), sda=Pin(12)))
-zsample = [0]*300
-ysample = [0]*300
-xsample = [0]*300
+class DataCollection: 
+    # Define the IMU 
+    lsm = LSM6DSOX(I2C(0, scl=Pin(13), sda=Pin(12)))
+    def __init__(self, duration=1000, filename="IMU_readings.csv"):
+        self.duration = duration  # set how long the IMU will record into the file, default 10 seconds
+        self.filename = filename # set where the IMU will record, default is IMU_readings.csv 
 
-# open the file in "append" mode so we don't overwrite prior runs
-filewrite = open("IMU_readings.csv", "a")
-# set a header to connect data to experiment realtime
-today = time.localtime()
-print(today)
-print("Current Time/Date", today, file=filewrite)
+    # open the file in "append" mode so we don't overwrite prior runs
+    filewrite = open(self.filename, "a")
 
-time.sleep_ms(500) # IMU startup time
-timecount = 0 # approximately ms
-IMUdatacollect = []
-while (timecount < 7000): #this does not have real units but about 2*ms runtime
-    gc.collect() # don't remember why we need to garbage collect every time
-    
-    # Collecting each of the values separately to make them easier to write/read as CSV
-    IMUDataX = lsm.read_accel()[0] # X acceleration data in m/s^2
-    IMUDataY = lsm.read_accel()[1] # Y acceleration data in m/s^2
-    IMUDataZ = lsm.read_accel()[2] # Z acceleration data in m/s^2
-    GyroDataX = lsm.read_gyro()[0] # gyro x in radians/second
-    GyroDataY = lsm.read_gyro()[0] # gyro Y in radians/second
-    GyroDataZ = lsm.read_gyro()[0] # gyro Z in radians/second
-#    print("Gyro data", GyroData)
-#    print("Acceleration data from timestep", timecount, "is", IMUdata)
+    # set a header to connect data to experiment realtime
+    today = time.localtime()
+    print(today) # printing to the REPL/terminal
+    print("Current Time/Date", today, file=filewrite) # print the header into the file
 
-# printing to file in CSV format, which we have to do by hand because micropython
-    print(timecount,",",IMUDataX,",", IMUDataY,",", IMUDataZ,",",  GyroDataX,",",  GyroDataY,",",  GyroDataZ, file=filewrite)
-    time.sleep_ms(10)
-    timecount = timecount+10
+    time.sleep_ms(500) # IMU startup time
 
 
+    def timeoutcallback(t): # when the timer hits the time set, stop the IMU
+        IMUSTOP==True
 
-filewrite.flush()
+    # one shot timer, firing after self.duration ms, using machine.Timer
+    IMUtimer.init(mode=Timer.ONE_SHOT, period=self.duration, callback=timeoutcallback)
+
+#    IMUdatacollect = []
+    while not IMUSTOP: 
+        gc.collect() # don't remember why we need to garbage collect every time
+        
+        # Collecting each of the values separately to make them easier to write/read as CSV
+        IMUDataX = lsm.read_accel()[0] # X acceleration data in m/s^2
+        IMUDataY = lsm.read_accel()[1] # Y acceleration data in m/s^2
+        IMUDataZ = lsm.read_accel()[2] # Z acceleration data in m/s^2
+        GyroDataX = lsm.read_gyro()[0] # gyro x in radians/second
+        GyroDataY = lsm.read_gyro()[0] # gyro Y in radians/second
+        GyroDataZ = lsm.read_gyro()[0] # gyro Z in radians/second
+    #    print("Gyro data", GyroData)
+    #    print("Acceleration data from timestep", timecount, "is", IMUdata)
+
+    # printing to file in CSV format, which we have to do by hand because micropython
+        print(timecount,",",IMUDataX,",", IMUDataY,",", IMUDataZ,",",  GyroDataX,",",  GyroDataY,",",  GyroDataZ, file=filewrite)
+        time.sleep_ms(10)
+
+
+
+    filewrite.flush()
 
 
---
+++ main.py
@@ -0,0 +1,67 @@
+# Complete project details at https://RandomNerdTutorials.com
+
+import wifimgr
+from time import sleep
+import machine
+
+try:
+  import usocket as socket
+except:
+  import socket
+
+led = machine.Pin(2, machine.Pin.OUT)
+
+wlan = wifimgr.get_connection()
+if wlan is None:
+    print("Could not initialize the network connection.")
+    while True:
+        pass  # you shall not pass :D
+
+# Main Code goes here, wlan is a working network.WLAN(STA_IF) instance.
+print("ESP OK")
+
+def web_page():
+  if led.value() == 1:
+    gpio_state="ON"
+  else:
+    gpio_state="OFF"
+  
+  html = open(mainpage.html)
+  return html
+  
+try:
+  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+  s.bind(('', 80))
+  s.listen(5)
+except OSError as e:
+  machine.reset()
+
+while True:
+  try:
+    if gc.mem_free() < 102000:
+      gc.collect()
+    conn, addr = s.accept()
+    conn.settimeout(3.0)
+    print('Got a connection from %s' % str(addr))
+    request = conn.recv(1024)
+    conn.settimeout(None)
+    request = str(request)
+    print('Content = %s' % request)
+    led_on = request.find('/?led=on')
+    led_off = request.find('/?led=off')
+    if led_on == 6:
+      print('LED ON')
+      led.value(1)
+    if led_off == 6:
+      print('LED OFF')
+      led.value(0)
+    response = web_page()
+    conn.send('HTTP/1.1 200 OK\n')
+    conn.send('Content-Type: text/html\n')
+    conn.send('Connection: close\n\n')
+    conn.sendall(response)
+    conn.close()
+  except OSError as e:
+    conn.close()
+    print('Connection closed')
