dce5c52238ecd3bd1ea555bbf9cb2d59211dc876
katallen @ jigglypuff <katallen@jigglypuff>
2024-10-15 16:33:43 -0400

    rosbag extract twist

--- processing/rosbag-extracttwist.py
+++ processing/rosbag-extracttwist.py
@@ -3,7 +3,9 @@ import os
 import csv
 import rosbag
 import rospy
-
+import datetime
+import time
+import glob
 ##################
 # DESCRIPTION:
 # Creates CSV files of the robot joint states from a rosbag (for visualization with e.g. pybullet)
@@ -12,37 +14,73 @@ import rospy
 # rosrun your_package get_jstate_csvs.py /root/catkin_ws/bagfiles your_bagfile.bag
 # ##################
 
-filename = sys.argv[2]
+#filename = sys.argv[2]
 directory = sys.argv[1]
-print("Reading the rosbag file")
-if not directory.endswith("/"):
-  directory += "/"
-extension = ""
-if not filename.endswith(".bag"):
-  extension = ".bag"
-bag = rosbag.Bag(directory + filename + extension)
 
-# Create directory with name filename (without extension)
-results_dir = directory + filename[:-4] + "_results"
-if not os.path.exists(results_dir):
-  os.makedirs(results_dir)
+def get_files(path, extension, recursive=False):
+    """
+    A generator of filepaths for each file into path with the target extension.
+    If recursive, it will loop over subfolders as well.
+    """
+    if not recursive:
+        for file_path in glob.iglob(path + "/*." + extension):
+            yield file_path
+    else:
+        for root, dirs, files in os.walk(path):
+            for file_path in glob.iglob(root + "/*." + extension):
+                print(file_path)
+                res = file_path.split('/')
+                global filename
+                global subdir
+                filename = res[-1]
+                subdir = res[-2]
+                print(filename)
+                yield file_path
 
-print("Writing robot joint state data to CSV")
-force = [0,0,0,0,0,0]
-with open(results_dir +"/"+filename+'_forcetorque-calibrated.csv', mode='w') as data_file:
-  data_writer = csv.writer(data_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-  data_writer.writerow(['time', 'forcex', 'forcey','forcez','torquex','torquey','torquez'])
-  # Get all message on the /ft_sensor/ft_compensated topic
-  for topic, msg, t in bag.read_messages(topics=['/ft_sensor/ft_compensated']):
-      t_epoch = msg.header.stamp.sec
-      t = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(t_epoch))
-      force[0]=msg.wrench.force.x
-      force[1]=msg.wrench.force.y
-      force[2]=msg.wrench.force.z
-      force[3]=msg.wrench.torque.x
-      force[4]=msg.wrench.torque.y
-      force[5]=msg.wrench.torque.z
-      data_writer.writerow([t, force[0], force[1], force[2], force[3], force[4], force[5], force[6]])
 
-print("Finished creating csv file!")
-bag.close()
+
+for f in get_files(directory, 'bag', recursive=True):
+  #print("Reading the rosbag file")
+  #if not directory.endswith("/"):
+  #  directory += "/"
+  #extension = ""
+  #if not filename.endswith(".bag"):
+  #  extension = ".bag"
+  #bag = rosbag.Bag(directory + filename + extension)
+  bag = rosbag.Bag(f)
+  
+  # Create directory with name filename (without extension)
+  results_dir = directory +'/'+'results_'+ filename[:-4] 
+  if not os.path.exists(results_dir):
+    os.makedirs(results_dir)
+    
+  print("Writing robot joint state data to CSV")
+  force = [0,0,0,0,0,0]
+  lastsecond = None
+  print("subdir", subdir)
+  with open(results_dir +"/"+filename+subdir+'_forcetorque-calibrated.csv', mode='w') as data_file:
+    data_writer = csv.writer(data_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    data_writer.writerow(['time', 'forcex', 'forcey','forcez','torquex','torquey','torquez'])
+    # Get all message on the /ft_sensor/ft_compensated topic
+    count = 1
+    for topic, msg, t in bag.read_messages(topics=['/ft_sensor/ft_compensated']):
+        t_epoch = msg.header.stamp.secs
+        
+        if time.localtime(t_epoch)[5] != lastsecond:
+          count = 1
+          lastsecond = time.localtime(t_epoch)[5]
+        else:
+          count = count+1
+          t = time.strftime("%a, %d %b %Y %H:%M:%S",
+                            time.localtime(t_epoch))
+          t +='.'+str(count)
+          force[0]=msg.wrench.force.x
+          force[1]=msg.wrench.force.y
+          force[2]=msg.wrench.force.z
+          force[3]=msg.wrench.torque.x
+          force[4]=msg.wrench.torque.y
+          force[5]=msg.wrench.torque.z
+          data_writer.writerow([t, force[0], force[1], force[2], force[3], force[4], force[5]])
+          
+  print("Finished creating csv file!")
+  bag.close()
--- video-manipulation/videoprocessing.py
+++ video-manipulation/videoprocessing.py
@@ -8,13 +8,9 @@ length_regexp = 'Duration: (\d{2}):(\d{2}):(\d{2})\.\d+,'
 re_length = re.compile(length_regexp)
 import datetime
 import sys
-def main(filename=None, CSVfilename=None):
-    if filename==None:
-        print("Filename to split?")
-        filename = str(input())
-        print("CSV with split times")
-        CSVfilename = str(input())
 
+
+def main(filename, CSVfilename, experimentnumber):
     print("Loading split times from", CSVfilename)
     cliptimes = []
     try:
@@ -24,61 +20,112 @@ def main(filename=None, CSVfilename=None):
                 if line.startswith("UTCtime"):
                     # pull out just the time
                     linelabel,linetime,linename = line.split('**')
+                    # strip extra commas from the name
                     linename = linename.replace(",","")
-                    print("start at", linetime, "name", linename)
+                    linename = linename.replace("\n","-")
+                    # strip spaced from the time
+                    linetime = linetime.replace(" ","")
+                    #print("start at", linetime, "name", linename)
                     # convert from string format to datetime format
+                    print("Line time is: ", linetime)
                     format = '%Y,%m,%d,%H:%M:%S'
                     thislinetime = datetime.datetime.strptime(linetime, format)
                     cliptimes.append([thislinetime,linename])
 
     except Exception as e:
-        print(e)
-    
-    if split_length <= 0:
-        print("Split length can't be 0")
-        raise SystemExit
-    output = subprocess.Popen("ffmpeg -i '"+filename+"' 2>&1 | grep 'Duration'",
-                              shell = True,
-                              stdout = subprocess.PIPE
-    ).stdout.read()
-    print(output)
-    matches = re_length.search(output)
-    if matches:
-        video_length = int(matches.group(1)) * 3600 + \
-                       int(matches.group(2)) * 60 + \
-                       int(matches.group(3))
-        print("Video length in seconds: "+str(video_length))
-    else:
-        print("Can't determine video length.")
-        raise SystemExit
-    split_count = int(math.ceil(video_length/float(split_length)))
-    if(split_count == 1):
-        print("Video length is less then the target split length.")
-        raise SystemExit
-    split_cmd = "ffmpeg -i '"+filename+"' -vcodec copy "
-    for n in range(0, split_count):
-        split_str = ""
-        if n == 0:
-            split_start = 0
-        else:
-            split_start = split_length * n
-            split_str += " -ss "+str(split_start)+" -t "+str(split_length) + \
-                         " '"+filename[:-4] + "-" + str(n) + "." + filename[-3:] + \
-                         "'"
-    print("About to run: "+split_cmd+split_str)
-    output = subprocess.Popen(split_cmd+split_str, shell = True, stdout =
-                              subprocess.PIPE).stdout.read()
+        print("an exception occurred", e)
 
+    print("all clip times", cliptimes)
+    print("start time for this video is: ", cliptimes[0])
+    videostarttime = cliptimes[0][0]
+    lastcliptime = videostarttime
+    for cliptime in cliptimes:
+        print("splitting off clip", cliptime[1], "utctime: ", cliptime[0])
+        timedelta = cliptime[0]-videostarttime
+        print(timedelta, "is the time from video start for clip", cliptime[1])
+        
+        # building up this shape of command:        
+        # ffmpeg -i topcamera.mkv -ss 00:00:30 -t 90 -c:v copy -c:a copy hleadertopcamera.mkv
+        outputfilename = cliptime[1]+filename
+        clipduration = (cliptime[0]-lastcliptime).total_seconds()
+        print("clip duration is ", clipduration)
+        if clipduration !=0: 
+            split_cmd = "ffmpeg -i "+ filename+ " -ss "+ str(timedelta)+" -t "+ str(clipduration) + " -c:v copy -c:a copy " +outputfilename
+            print("About to run: ", split_cmd)
+            input()
+            output = subprocess.Popen(split_cmd, shell = True, stdout =
+                                          subprocess.PIPE).stdout.read()
+        lastcliptime = cliptime[0] 
+
+        
+def get_cardorder(experimentnumber=""):
+    if experimentnumber == "":
+        got_experimentnumber=False
+        while not got_experimentnumber:
+            print("Experiment Number:")
+            try:
+                experimentnumber = int(input())
+                got_experimentnumber = True
+            except:
+                print("Experiment Number (integer)")
+                experimentnumber = int(input())
+    try:
+        filename = "/home/katallen/workspace/src/hapticcomm/experiment_card_full_order.txt"
+        cardset = experimentnumber % 6
+        print("cardset is ", cardset)
+        print("Using Card Set", cardset)
+        with open(filename, 'r') as f:
+            filelist = csv.DictReader(f)    
+            cardsorts = [row for row in filelist] #all the card orders
+            # just this experiment's cards
+            card_dictionary = cardsorts[cardset-1]
+            cards = list(card_dictionary.values()) 
+            print("\n\n\nThis experiment's cards are, ", cards)
+            return cards
+    except Exception as e: 
+        print("cards not set, ", e)
+        exit
+
+
+def add_cardnames_to_csv(CSVfilename, cards):
+    count = 0
+    newCSVfilename = "marked"+CSVfilename
+    with open(CSVfilename, 'r') as f:
+        for line in f:
+            if line.startswith("UTCtime"):
+                print(line)
+                print(cards)
+                line = line+cards[count]
+                count = (count+1) % 6
+            with open(newCSVfilename, 'w') as g:
+                writer_object = csv.writer(g, delimiter=',')
+                print("The line about to be written is", line)
+                writer_object.writerow(line)
+
+
+
+    
 if __name__ == '__main__':
     try:
         filename = sys.argv[1]
         CSVfilename = sys.argv[2]
+        experimentnumber = sys.argv[3]
     except Exception as e:
         print(e)
-        filename=None
-        CSVfilename=None
+        print("Filename to split?")
+        filename = str(input())
+        print("CSV with split times")
+        CSVfilename = str(input())
+        print("Experiment Number?")
+        experimentnumber = str(input())
+
+    print("add names to CSV(1) or main?(2)")
+    choice = int(input())
+    if choice == 1:
+        cards = get_cardorder()
+        add_cardnames_to_csv(CSVfilename, cards)
     try:
-        main(filename,CSVfilename)
+        main(filename,CSVfilename, experimentnumber)
     except Exception as e:
         print("Exception occured running main():")
         print(str(e))
